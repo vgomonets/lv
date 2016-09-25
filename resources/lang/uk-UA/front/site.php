df4cb1565f5bdfbec99b5527e66069158f996476
vgomonets <vgomonets@i.ua>
2016-09-25 13:57:52 +0300

    add uk-UA locale

---
+++ resources/lang/uk-UA/front/site.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Laravel 5',
+    'sub-title' => 'An awesome PHP framework',
+    'home' => 'Домівка',
+    'blog' => 'Blog',
+    'register' => 'Register',
+    'forget-password' => 'Forgotten password',
+    'connection' => 'Connection',
+    'administration' => 'Administration',
+    'redaction' => 'Редакція',
+    'logout' => 'Logout',
+    'notification-endtext' => 'If you’re having trouble clicking the button, copy and paste the URL below into your web browser:',
+    'salutation' => 'Regards',
+    'yes' => 'Yes',
+    'no' => 'No',
+];
